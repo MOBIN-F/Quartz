0ecc3e16852b5a4bd37ad592443f0b4bcc8d08b7
mobin <996833461@qq.com>
2016-03-14 16:20:10 +0800

    Job

---
+++ src/main/java/com/mobin/spring/quartz/MytaskJob.java
@@ -0,0 +1,12 @@
+package com.mobin.spring.quartz;
+
+/**
+ * Created by Mobin on 2016/3/14.
+ */
+public class MytaskJob {
+
+    public void doIt(){
+        //编写调度任务
+        System.out.println("开始执行任务！！！");
+    }
+}
